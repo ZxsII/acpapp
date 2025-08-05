0f46604120b8ec2dd2b17d2cee79d766fc12250e
ZxsII <67011268@kmitl.ac.th>
2025-08-05 14:18:34 +0700

    AdvcomproLab06

--- fastapi/database.py
+++ fastapi/database.py
@@ -1,17 +1,22 @@
 from databases import Database
 
+
 POSTGRES_USER = "temp"
 POSTGRES_PASSWORD = "temp"
 POSTGRES_DB = "advcompro"
 POSTGRES_HOST = "db"
 
+
 DATABASE_URL = f'postgresql+asyncpg://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}/{POSTGRES_DB}'
 
+
 database = Database(DATABASE_URL)
+
 
 async def connect_db():
     await database.connect()
     print("Database connected")
+
 
 async def disconnect_db():
     await database.disconnect()
@@ -28,15 +33,23 @@ async def insert_user(username: str, password_hash: str, email: str):
     values = {"username": username, "password_hash": password_hash, "email": email}
     return await database.fetch_one(query=query, values=values)
 
+
 # Function to select a user by user_id from the users table
-async def get_user(user_id: int):
-    query = "SELECT * FROM users WHERE user_id = :user_id"
-    return await database.fetch_one(query=query, values={"user_id": user_id})
+async def get_user(username: str):
+    query = "SELECT * FROM users WHERE username = :username"
+    return await database.fetch_one(query=query, values={"username": username})
+
+
+# Function to select a user by email from the users table
+async def get_user_by_email(email: str,password_hash:str):
+    query = "SELECT * FROM users WHERE email = :email and password_hash = :password_hash"
+    return await database.fetch_one(query=query, values={"email": email,"password_hash": password_hash})
+
 
 # Function to update a user in the users table
 async def update_user(user_id: int, username: str, password_hash: str, email: str):
     query = """
-    UPDATE users 
+    UPDATE users
     SET username = :username, password_hash = :password_hash, email = :email
     WHERE user_id = :user_id
     RETURNING user_id, username, password_hash, email, created_at
@@ -44,9 +57,9 @@ async def update_user(user_id: int, username: str, password_hash: str, email: st
     values = {"user_id": user_id, "username": username, "password_hash": password_hash, "email": email}
     return await database.fetch_one(query=query, values=values)
 
+
 # Function to delete a user from the users table
 async def delete_user(user_id: int):
     query = "DELETE FROM users WHERE user_id = :user_id RETURNING *"
     return await database.fetch_one(query=query, values={"user_id": user_id})
-
 
